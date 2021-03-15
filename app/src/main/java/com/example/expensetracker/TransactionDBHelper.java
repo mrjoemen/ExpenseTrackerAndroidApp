3165e4894c837df3056b9b317c63f0397931aa7c
ntomsky <nikita.tomsky@gmail.com>
2021-03-15 12:40:02 -0400

    added TransactionDBHelper class

---
+++ app/src/main/java/com/example/expensetracker/TransactionDBHelper.java
@@ -0,0 +1,35 @@
+package com.example.expensetracker;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class TransactionDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "transactions.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //Database creation sql statement
+    private static final String CREATE_TABLE_TRANSACTIONS =
+            "create table transactions (_id integer primary key autoincrement, " +
+                    "transactionAmount float, transactionDate text, description text);";
+
+    public TransactionDBHelper (Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_TRANSACTIONS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TransactionDBHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to " + newVersion +
+                "which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS transactions");
+        onCreate(db);
+    }
+}
