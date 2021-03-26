a9ec0c34d5d52ad2a7edf207936b35c0d884a287
ntomsky <nikita.tomsky@gmail.com>
2021-03-26 14:39:24 -0400

    added inner class in InvestementOptions class to pull SPY quote from MarketWatch and displaying it

--- app/src/main/java/com/example/expensetracker/InvestmentOptions.java
+++ app/src/main/java/com/example/expensetracker/InvestmentOptions.java
@@ -1,19 +1,31 @@
 package com.example.expensetracker;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InvestmentOptions extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
+public class InvestmentOptions extends AppCompatActivity {
+    TextView SPY_TextView;
+    final String SPY_url = UrlBuilder("SPY");
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_investments);
         initMenuItems();
+        SPY_TextView = findViewById(R.id.TikPrice);
+
+        //calling for inner class passing URL for the stock
+        new MyTask().execute(SPY_url);
     }
 
     private void initMenuItems(){
@@ -24,5 +36,56 @@ public class InvestmentOptions extends AppCompatActivity {
             Intent intent = new Intent(InvestmentOptions.this, MainActivity.class);
             startActivity(intent);
         });
+    }
+
+    //class to call url Asyncronously where we pass the url and the TextView item to be set with the returned price
+    private class MyTask extends AsyncTask<Object, Void, Void> {
+       String quote = "not found";
+       TextView tv;
+
+        //call url
+        @Override
+        protected Void doInBackground(Object... link) {
+
+            try {
+                URL url = new URL((String)link[0]);// assigning passed url for HTTP call
+                URLConnection urlConnection = url.openConnection();
+                InputStreamReader inStream = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader buff = new BufferedReader(inStream);
+
+                String line = buff.readLine();
+
+                //looping over results
+                while (line != null) {
+                    //once found the segment, pull the price
+                    if (line.contains("price\":\"")) {
+                        int target = line.indexOf("price\":\"");
+                        int decimal = line.indexOf(".", target);
+                        int start = decimal;
+                        while (line.charAt(start) != '\"') {
+                            start--;
+                        }
+
+                        //assign the result to global var quote
+                        quote = line.substring(start + 1, decimal + 3);
+                        break;
+                    }
+                    line = buff.readLine();
+                }
+            }
+                        catch (Exception e){
+
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void price) {
+            //set the above global TextView to the pulled quote, found in doInBackground
+            SPY_TextView.setText("SPY today is $"+ quote);
+        }
+    }
+
+    protected String UrlBuilder(String stockSym){
+        return "https://www.marketwatch.com/investing/fund/" + stockSym + "/download-data?siteid=mktw&date=&x=0&y=0";
     }
 }
