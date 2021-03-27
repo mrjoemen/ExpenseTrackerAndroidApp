81ef9f143bdfc51015bf4a4cd61c6a513eeb5fae
NapSo <dustin.willingh4m@gmail.com>
2021-03-26 20:16:44 -0400

    SPY able to purchase added. BTC noted out

--- app/src/main/java/com/example/expensetracker/InvestmentOptions.java
+++ app/src/main/java/com/example/expensetracker/InvestmentOptions.java
@@ -15,19 +15,28 @@ import java.net.URLConnection;
 
 public class InvestmentOptions extends AppCompatActivity {
     TextView SPY_TextView;
+    TextView BTC_TextView;
+    TextView Stocks_Afforded;
     final String SPY_url = NYSurlBuilder("SPY");
-    final String BTC_url = criptoURLBuilder("bitcoin");
+    //final String BTC_url = criptoURLBuilder("BTCUSD");
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_investments);
         initMenuItems();
+        initCurrentBalance();
         SPY_TextView = findViewById(R.id.TikPrice);
         SPY_TextView.setText("Loading...");
+        BTC_TextView = findViewById(R.id.btcPrice);
+        BTC_TextView.setText("Loading...");
+
+        Stocks_Afforded = findViewById(R.id.textAfforded);
 
         //calling for inner class passing URL for the stock
         new MyTask().execute(SPY_url);
+       // new MyTask().execute(BTC_url);
+
     }
 
     private void initMenuItems(){
@@ -38,6 +47,13 @@ public class InvestmentOptions extends AppCompatActivity {
             Intent intent = new Intent(InvestmentOptions.this, MainActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void initCurrentBalance(){
+        //ds instance for accessing instance method later
+        TransactionDataSource dataSource = new TransactionDataSource(InvestmentOptions.this);
+        TextView balance = findViewById(R.id.dollarText);
+        balance.setText("$" + dataSource.getBalance());
     }
 
     //class to call url Asyncronously where we pass the url and the TextView item to be set with the returned price
@@ -84,14 +100,25 @@ public class InvestmentOptions extends AppCompatActivity {
         protected void onPostExecute(Void price) {
             //set the above global TextView to the pulled quote, found in doInBackground
             SPY_TextView.setText("SPY today is $"+ quote);
+            double stockPrice = Double.parseDouble(quote);
+            TransactionDataSource dataSource = new TransactionDataSource(InvestmentOptions.this);
+            Double balance = Double.valueOf(dataSource.getBalance());
+            double afforded = balance / stockPrice;
+            Stocks_Afforded.setText("Number of shares you could purchase:" + afforded);
         }
+
+        //protected void onPostExecuteBtc(Void price){
+      //  BTC_TextView.setText("BTC today is $" + quote);
+       // }
+
     }
 
-    protected String NYSurlBuilder(String stockSym){
+    protected String NYSurlBuilder(String stockSym) {
         return "https://www.marketwatch.com/investing/fund/" + stockSym + "/download-data?siteid=mktw&date=&x=0&y=0";
     }
-    protected String criptoURLBuilder(String currencyName){
 
-        return "https://www.coindesk.com/price/" + currencyName;
-    }
+    /*protected String criptoURLBuilder(String currencyName){
+
+        return "https://www.marketwatch.com/investing/fund/" + currencyName + "/download-data?siteid=mktw&date=&x=0&y=0";
+    }*/
 }
